80b3feb1c0c04b88eb69258ab5a2988775d98752
Crelia, Rick <Rick.Crelia@exchange.nordstrom.com>
2014-10-09 15:00:28 -0700

    week1 homework

--- week1/homework/strings_and_rspec_spec.rb
+++ week1/homework/strings_and_rspec_spec.rb
@@ -1,7 +1,7 @@
+# encoding: utf-8
 require 'rspec/collection_matchers'
 require_relative '../../spec_helper'
 
-# encoding: utf-8
 
 # Please make these examples all pass
 # You will need to change the 3 pending tests
@@ -16,18 +16,20 @@ describe String do
 			@my_string = "Renée is a fun teacher. Ruby is a really cool programming language"
 		end
 
-		it "should be able to count the charaters"
+		it "should be able to count the charaters" do
+			@my_string.length.should eq 66
+		end
 
 		it "should be able to split on the . charater" do
-			pending
-			result = #do something with @my_string here
+			result = @my_string.split('.')
 			result.should have(2).items
 		end
 
 		it "should be able to give the encoding of the string" do
-			pending 'helpful hint: should eq (Encoding.find("UTF-8"))'
-			encodeing #do something with @my_string here
+			#pending 'helpful hint: should eq (Encoding.find("UTF-8"))'
+			#encodeing #do something with @my_string here
 			#use helpful hint here
+			@my_string.encoding.should == Encoding.find("UTF-8")
 		end
 	end
 end
