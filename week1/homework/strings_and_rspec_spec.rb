2c1da72bab6b49e28fecaed7c35cc34b6962f65b
Alicia Goodwin <aliciagoodinwin@gmail.com>
2014-10-09 10:34:43 -0700

    Rspec test. I understand the second two but not the first. will ask in class

--- week1/homework/strings_and_rspec_spec.rb
+++ week1/homework/strings_and_rspec_spec.rb
@@ -11,24 +11,30 @@ require_relative '../../spec_helper'
 # 	(Hint: You should do the reading on Strings first)
 
 describe String do
-	context "When a string is defined" do
-		before(:all) do
-			@my_string = "Renée is a fun teacher. Ruby is a really cool programming language"
-		end
+  context "When a string is defined" do
+    before(:all) do
+      @my_string = "Renee is a fun teacher. Ruby is a really cool programming language"
+    end
+    
+    it "should be able to count the characters" do
+      "Renee is a fun teacher. Ruby is a really cool programming language".should have(66).characters
+    end
 
-		it "should be able to count the charaters"
+    it "should be able to split on the . character" do
+      #pending
+      result = @my_string.split(/\s*\.\s*/)
+      #do something with @my_string here
+      result.should have(2).items
+    end
 
-		it "should be able to split on the . charater" do
-			pending
-			result = #do something with @my_string here
-			result.should have(2).items
-		end
-
-		it "should be able to give the encoding of the string" do
-			pending 'helpful hint: should eq (Encoding.find("UTF-8"))'
-			encodeing #do something with @my_string here
-			#use helpful hint here
-		end
-	end
+    it "should be able to give the encoding of the string" do
+      #pending 'helpful hint: should eq (Encoding.find("UTF-8"))'
+      # I have ruby 1.9 so the encoding on my machine is US-ASCII
+      encoding = @my_string.encoding
+      #do something with @my_string here. 
+      
+      encoding.should eq Encoding.find("US-ASCII")
+    end
+  end
 end
 
