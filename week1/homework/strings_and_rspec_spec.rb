ea05488f2cfb03be7c8fe4fe10e7d06c21db3ced
Emily <metroretro@users.noreply.github.com>
2014-10-09 18:30:21 -0700

    Update strings_and_rspec_spec.rb

--- week1/homework/strings_and_rspec_spec.rb
+++ week1/homework/strings_and_rspec_spec.rb
@@ -12,23 +12,29 @@ require_relative '../../spec_helper'
 
 describe String do
 	context "When a string is defined" do
+		#before(:all) >> runs once before everything else runs. Good for if you have setup
 		before(:all) do
 			@my_string = "Renee is a fun teacher. Ruby is a really cool programming language"
 		end
 
 		it "should be able to count the characters" do
 			result = @my_string.length
-			result.should eq 66		
+			result.should eq 66
+			#expect(@my_string.size).to eq 66
+			#@my_string.size.should eq 66
 		end
 
 		it "should be able to split on the . character" do
 			result = @my_string.split(/\./)
+			#result = @my_string.split('.')
 			result.should have(2).items
+			#rspec calls .items on collection matchers, returns an array
 		end
 
 		it "should be able to give the encoding of the string" do
 			result = @my_string.encoding
 			result.should eq (Encoding.find("UTF-8"))
+			#Encoding is a class, has method .find for finding encoding object
 		end
 	end
 end
