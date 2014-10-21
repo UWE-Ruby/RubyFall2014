31de8e4c5ffc982e9846628ae8a219eb24061981
Jon Parten <jon.parten@gmail.com>
2014-10-20 22:37:54 -0700

    converting from tabs to spaces

--- week3/homework/calculator.rb
+++ week3/homework/calculator.rb
@@ -1,24 +1,24 @@
 class Calculator
-	def sum(values)
-		values.inject(0) { |result, next_val| result + next_val }
-	end
-	def multiply(*values)
+  def sum(values)
+    values.inject(0) { |result, next_val| result + next_val }
+  end
+  def multiply(*values)
     values_list = Array.new
-		if (values.first.kind_of?(Array))
-			values_list += values.first
-		else
-			values_list += values
-		end
-		values_list.inject { |result, next_val| result * next_val }
-	end
+    if (values.first.kind_of?(Array))
+      values_list += values.first
+    else
+      values_list += values
+    end
+    values_list.inject { |result, next_val| result * next_val }
+  end
 
-	def pow(base, exponent)
-		result = 1
-		exponent.times{ result *= base }
-		result
-	end
+  def pow(base, exponent)
+    result = 1
+    exponent.times{ result *= base }
+    result
+  end
 
-	def fac(value)
-		value == 0 ? 1 : (1..value).inject { |result, next_val| result * next_val }
-	end
+  def fac(value)
+    value == 0 ? 1 : (1..value).inject { |result, next_val| result * next_val }
+  end
 end
