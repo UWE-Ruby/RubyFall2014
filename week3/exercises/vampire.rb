85412818a27490866c504f253e55ba1056e62470
Emily Ruf <emxruf@gmail.com>
2014-10-22 20:00:06 -0700

    Week 3 homework and exercises

--- week3/exercises/vampire.rb
+++ week3/exercises/vampire.rb
@@ -1,6 +1,10 @@
 require './monster.rb'
-class Vampire < Monster
+class Vampire < Monster # inheriting from monster
+	#override inherited initialize functionality
+	#set up logical defaults that are *slightly* different from Monster superclass
 	def initialize(noc=true, legs=2, name ="Vampire", vul=[:garlic, :sunlight], dangers=[:bites])
+		#super - call back to Monster superclass.
+		#call this method (inititalize) on parent class Monster, and use these args (same number, same order, passes args to parent method)
 		super(noc,legs,name,vul,dangers)
 	end
 end
