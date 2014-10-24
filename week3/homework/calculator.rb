d58c75f63c03cb32726e105bb35194142b1bb515
syearva <mohan.surendra@gmail.com>
2014-10-23 19:02:31 -0700

    Update calculator.rb

--- week3/homework/calculator.rb
+++ week3/homework/calculator.rb
@@ -1,9 +1,26 @@
 class Calculator
 
-	def multiply(*numbers)
+	def sum input
+		
+		total = 0
+		input.each { |a| total += a }
+		end
+		total
+	end
+	
+	def multiply *numbers
  		result = 1
-  		numbers.each { |n| result = result * n }
+  		numbers.flatten.each { |m| result *= m }
   		result
 	end
+	
+	def pow base,exp
+		base ** exp
+	end
+	
+	def fac n
+		retun 1 if n.zero?
+		n *= fac(n-1)
+	end 
 	
 end
