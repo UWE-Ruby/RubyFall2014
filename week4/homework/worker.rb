9abe5865200c367bc8c3693ec1b11f2f377bd0b3
Rick Crelia <rcrelia@gmail.com>
2014-10-28 20:19:01 -0700

    all 4 tests pass

--- week4/homework/worker.rb
+++ week4/homework/worker.rb
@@ -1,7 +1,17 @@
-class Worker
+module Worker
 
-  def Worker.work
-      yield if block_given?
-  end
+#  def Worker.work
+#      yield if block_given?
+#  end
 
+   def self.work(*args)
+       c = 0
+       if args[0] == nil
+          yield if block_given?
+       else
+          yield if block_given?
+          yield if block_given?
+          yield if block_given?
+       end
+   end
 end
