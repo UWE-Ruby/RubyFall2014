cdc86083e150b708ea5ecbd11714d79077da9f1d
ruby -v <mohan.surendra@gmail.com>
2014-10-30 15:27:28 -0700

    committing

--- week4/homework/worker.rb
+++ week4/homework/worker.rb
@@ -1,42 +1,10 @@
-
-#module Greet
-#	def work 
-#  		puts "hello"
-#	end
-#end
-
-#class Worker
-#	include Greet
-# 	def work input
-# 		puts input
-#	end
-#end
-
-#Worker_obj = Worker.new  
-#Worker_obj.work("Hello")
-#module Worker
-#	def work
-#		puts "Hello122"
-#	end
-#end
-#Worker.work
-
-
-
-#module Worker
-
-#def self.work x=1
-#  output = ""
-#  x.times {output = yield}
-#  output
-#end
-
-#end
+###
 
 module Worker 
-	def self.work x=1
-		value = ''
-		x.times { value = yield }
+	def self.work
+		value = yield 
 		value
 	end
 end
+
+## Still working on passing parameter.
