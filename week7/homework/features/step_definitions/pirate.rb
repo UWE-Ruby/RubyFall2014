063a7ef325b42d7097be9cdd45b4bbcbc5c53f07
Katie Atrops <katrops@gmail.com>
2014-11-20 13:41:44 -0800

    to add homework for week7

---
+++ week7/homework/features/step_definitions/pirate.rb
@@ -0,0 +1,11 @@
+class PirateTranslator
+  
+  def say(arg)
+      @hi = "Ahoy Matey"
+      return @hi
+  end
+  
+  def translate
+    return "#{@hi}\n Shiber Me Timbers You Scurvey Dogs!!"
+  end
+end
