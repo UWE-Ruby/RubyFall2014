f818ab408e755c20a8e59b2ddd36af3f40763bfa
Joseph Simpson <jjs0sbw@gmail.com>
2014-11-29 19:17:52 -0800

    create new final directory

---
+++ final/features/step_definitions/tic_tac_toe.rb
@@ -0,0 +1,36 @@
+#UW Ruby class Fall 2014 joseph Simpson
+class TicTacToe
+  SYMBOLS = [:X, :O]
+  attr_accessor :player
+  
+  def initialize(player = nil, player_symbol=nil)
+    @player = player
+    @player_symbol = player_symbol
+    @board
+  end
+
+  def welcome_player
+    "Welcome #{@player}"
+  end
+  
+  def current_player
+    @player
+  end
+
+  def player_symbol
+    SYMBOLS[0]
+  end
+
+  def computer_symbol
+    SYMBOLS[1]
+  end
+
+  def indicate_player_turn
+    puts "#{@player}'s Move:"
+  end
+
+  def get_player_move
+
+  end
+
+end
